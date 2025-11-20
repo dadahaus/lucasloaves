111c23ef449f5a0bac09ceb741b4abed68532c52
ostin <casadadaista@gmail.com>
2025-11-20 17:42:53 +1100

    loaves

---
+++ processConnectDB.php
@@ -0,0 +1,15 @@
+<?php
+// Processes/processConnectDB.php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "dbLucasLoaves"; // Changed to match your SQL file
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
